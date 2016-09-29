d367d949ba80d6c25a18e88e8590989d835019e5
App Academy Student <student@appacademy.io>
2016-09-29 16:09:41 -0700

    adds polls, at poll results

--- PollApp/app/models/question.rb
+++ PollApp/app/models/question.rb
@@ -18,9 +18,32 @@ class Question < ActiveRecord::Base
     foreign_key: :poll_id,
     class_name: :Poll
 
-    has_many :answer_choices,
-      primary_key: :id,
-      foreign_key: :question_id,
-      class_name: :AnswerChoice
+  has_many :answer_choices,
+    primary_key: :id,
+    foreign_key: :question_id,
+    class_name: :AnswerChoice
+
+  has_many :responses,
+    through: :answer_choices,
+    source: :response
+
+  def results
+    # count = Hash.new(0)
+    # self.answer_choices.each do |answer_choice|
+    #   if !answer_choice.response.nil?
+    #     count[answer_choice.answer] += 1
+    #   end
+    # end
+    # count
+
+    count = Hash.new(0)
+    answer_choices = self.answer_choices.includes(:response).where(answer_choice_id: self.answer_choices.id)
+    p answer_choices
+    # answer_choices.each do |answer_choice|
+    #   # if !answer_choice.response.nil?
+    #   count[answer_choice.response] +=1
+    # end
+    # count
+  end
 
 end
--- PollApp/app/models/response.rb
+++ PollApp/app/models/response.rb
@@ -12,6 +12,8 @@
 class Response < ActiveRecord::Base
   validates :user_id, presence: true
   validates :answer_choice_id, presence: true
+  validate :respondent_already_answered?
+  validate :author_cant_respond
 
   belongs_to :user,
     primary_key: :id,
@@ -21,6 +23,30 @@ class Response < ActiveRecord::Base
   belongs_to :answer_choice,
     primary_key: :id,
     foreign_key: :answer_choice_id,
-    class_name: :Response
+    class_name: :AnswerChoice
+
+  has_one :question,
+    through: :answer_choice,
+    source: :question
+
+  def author_cant_respond
+    if self.answer_choice.question.poll.author_id == self.user_id#where(author_id: self.user_id).exists?
+      self.errors[:author] << "can't respond to own poll"
+    end
+  end
+
+  def sibling_responses
+
+    result = self.question.responses.where.not(id: self.id)
+    # question = self.answer_choice.question
+    #
+    # question.answer_choices.includes(:response)
+  end
+
+  def respondent_already_answered?
+    if self.sibling_responses.exists?(user_id: self.user_id)
+      self.errors[:response] << "already exists for this question"
+    end
+  end
 
 end
